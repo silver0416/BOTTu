8b1e95c511ae97bf1e6d05146c80c4af93bf8548
Silver_Hsiao <silver041692@gmail.com>
2023-01-07 22:26:53 +0800

    set discord channel ID in telegram chat room.

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ DISCORD_CHANNEL_ID = secret['DISCORD_CHANNEL_ID']
 # ffmpeg path
 FFMPEG_PATH = 'ffmpeg.exe'
 
-def handle_telegram_sticker(sticker_id):
+def handle_telegram_sticker(sticker_id,chat_id):
     # Download the sticker
     r = requests.get('https://api.telegram.org/bot{}/getFile?file_id={}'.format(BOT_TOKEN, sticker_id), stream=True)
     if r.status_code != 200:
@@ -44,15 +44,37 @@ def handle_telegram_sticker(sticker_id):
 
     @client.event
     async def on_ready():
-        channel = client.get_channel(DISCORD_CHANNEL_ID)
-        await channel.send(file=sticker_file)
-        await client.close()
+        try:
+            channel = client.get_channel(DISCORD_CHANNEL_ID)
+            await channel.send(file=sticker_file)
+            await client.close()
+        except Exception as e:
+            bot.sendMessage(chat_id, 'Error: Failed to send sticker to Discord. Please check the channel ID.')
+            await client.close()
+            
     client.run(DISCORD_TOKEN)
 
 def handle_telegram_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == 'sticker':
-        handle_telegram_sticker(msg['sticker']['file_id'])
+        handle_telegram_sticker(msg['sticker']['file_id'],chat_id)
+    elif content_type == 'text':
+        command = msg['text'].split()[0]
+        if command == '/start':
+            bot.sendMessage(chat_id, 'Hello! I am a sticker bot. Send me a sticker and I will send it to Discord.\n'+
+                                        'send /set to set the Discord channel id to send the sticker to.\n'+
+                                        'send /nowID to get the current Discord channel id.')
+        elif command == '/set':
+            if len(msg['text'].split()) != 2:
+                bot.sendMessage(chat_id, 'Error: Invalid command. Please send /set DISCORD_CHANNEL_ID')
+                return
+            global DISCORD_CHANNEL_ID 
+            DISCORD_CHANNEL_ID = int(msg['text'].split()[1])
+            bot.sendMessage(chat_id, 'Discord channel ID set to {}'.format(DISCORD_CHANNEL_ID))
+        elif command == '/nowID':
+            bot.sendMessage(chat_id, 'Current Discord channel ID is {}'.format(DISCORD_CHANNEL_ID))
+        else:
+            bot.sendMessage(chat_id, 'I only accept stickers.')
 
 # Set up the Telegram bot
 bot = telepot.Bot(BOT_TOKEN)
