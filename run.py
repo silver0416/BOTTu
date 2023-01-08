b2837068622e271a862fc94da547765137837f26
Silver_Hsiao <silver041692@gmail.com>
2023-01-08 11:54:01 +0800

    adjust git pull command

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import sys
 python_path = sys.executable
 print('Python path:', python_path)
 
-subprocess.run(['git', 'pull'])
+subprocess.run(['git', '-c', 'diff.mnemonicprefix=false', '-c', 'core.quotepath=false', '--no-optional-locks', 'pull', 'origin', 'main'])
 subprocess.run([python_path, '-m', 'pip', 'install', '-r', 'requirements.txt'])
 
 
