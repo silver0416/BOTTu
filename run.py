fab623c1cc781b4b1e71469641b4e601d2bd6452
Silver_Hsiao <silver041692@gmail.com>
2023-04-09 19:08:27 +0800

    update launch check

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import shutil
 
 
 python_path = sys.executable
@@ -12,11 +13,11 @@ subprocess.run([python_path, '-m', 'pip', 'install', '-r', 'requirements.txt'])
 # check if the operating system is linux,if it is,then check if FFMPEG and gifski are installed
 if os.name == 'posix':
     # check if FFMPEG is installed
-    if not os.path.isfile('/usr/bin/ffmpeg'):
+    if not shutil.which('ffmpeg'):
         print('FFMPEG is not installed, please install it first!')
         exit()
     # check if gifski is installed
-    if not (os.path.isfile('/usr/bin/gifski') and os.path.isfile('/snap/bin/gifski')):
+    if not shutil.which('gifski'):
         print('gifski is not installed, please install it first!')
         exit()
 
